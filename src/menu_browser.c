7afb455a8474b1de204a62f38e09455e5cac5cc3
Ryan Carlson <ryan.austin.carlson@gmail.com>
2016-01-18 16:48:47 -0500

    got rid of horizontal scrolling code (finally), fewer debug statements

--- src/menu_browser.c
+++ src/menu_browser.c
@@ -19,10 +19,6 @@
 #define MIN_ROW_HEIGHT 44
 #define MAX_ROW_HEIGHT (int)MIN_ROW_HEIGHT * 2.5
 
-static const bool ENABLE_HORIZONTAL_SCROLLING = false;
-
-static void selected_index_monitor(void *data);
-
 // contains info for routes, direction, stops, predictions
 static MenuBrowser **s_menu_browsers = NULL;
 static int s_browser_index;
@@ -33,16 +29,6 @@ static TextLayer *s_text_layer_noresults = NULL;
 static AppTimer *s_timer = NULL; // timer that calls a fcn to send the first app msg
 static bool s_timer_fired = false; // true if the timer has already fired
 static int s_timer_browser_index = -1; // the browser index that we expect when the timer fires
-
-static AppTimer *s_horiz_scroll_timer = NULL;
-static bool s_horiz_scroll_timer_active = false; // true if timer has been scheduled
-static unsigned long s_horiz_scroll_timer_timestamp = 0; // in milliseconds
-static int s_horiz_scroll_offset = 0;
-static bool s_horiz_scroll_scrolling_still_required = true;
-static int s_horiz_scroll_menu_index = -1;
-// static bool s_horiz_scroll_menu_reloading_to_scroll = false;
-
-static int s_menu_selection_index = -1; // our currently selected item (row only for now)
 
 /*
  * Utilty Functions
@@ -221,41 +207,6 @@ static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, ui
   menu_cell_basic_header_draw(ctx, cell_layer, header);
 }
 
-static char *get_menu_item_text(MenuLayer *menu_layer, int index, char *text)
-{
-  char *modified = text;
-
-  MenuIndex menu_index = menu_layer_get_selected_index(menu_layer);
-  if (modified && menu_index.row == index)
-  {
-    int len = strlen(modified);
-
-    // if (len - HORIZ_SCROLL_VISIBLE_CHARS - s_horiz_scroll_offset > 0)
-    bool is_text_too_long = len - HORIZ_SCROLL_VISIBLE_CHARS > 0;
-
-    int offset_strlen = len - s_horiz_scroll_offset;
-    bool keep_scrolling = offset_strlen >= 0;
-    if (is_text_too_long && keep_scrolling)
-    {
-      if (offset_strlen == 0)
-      {
-        modified = "";
-        s_horiz_scroll_scrolling_still_required = true;
-      }
-      else if (offset_strlen == -1)
-      {
-        modified = text;
-      }
-      else
-      {
-        modified += s_horiz_scroll_offset;
-        s_horiz_scroll_scrolling_still_required = true;
-      }
-    }
-  }
-  return modified;
-}
-
 static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data)
 {
   MenuBrowser *browser = s_menu_browsers[s_browser_index];
@@ -263,172 +214,23 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
   char *title = browser->menu_titles[cell_index->row];
   char *subtitle = browser->menu_subtitles[cell_index->row];
 
-  char *newtitle = get_menu_item_text(browser->menu_layer, cell_index->row, title);
-
-  // menu_cell_basic_draw(ctx, cell_layer, newtitle, subtitle, NULL);
-
   GRect bounds = layer_get_frame(cell_layer);
-  // printf("origin: (%d, %d), size: (%d, %d)", bounds.origin.x, bounds.origin.y, bounds.size.w, bounds.size.h);
 
   int title_height = browser->menu_title_heights[cell_index->row];
   int subtitle_height = browser->menu_subtitle_heights[cell_index->row];
   int cell_width = bounds.size.w - 10;
 
-  graphics_draw_text(ctx, newtitle,
+  graphics_draw_text(ctx, title,
     fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), GRect(5, 0, cell_width, title_height),
     GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
-    graphics_draw_text(ctx, subtitle,
-      fonts_get_system_font(FONT_KEY_GOTHIC_24), GRect(5, title_height + 1, cell_width, subtitle_height),
-      GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
-}
 
-static void horiz_scroll_callback(void *data)
-{
-  if (!ENABLE_HORIZONTAL_SCROLLING)
-  {
-    return;
-  }
-
-  // printf("in horiz scroll callback");
-  s_horiz_scroll_timer = NULL;
-  s_horiz_scroll_offset++;
-
-  MenuBrowser *browser = s_menu_browsers[s_browser_index];
-  MenuLayer *menu_layer = NULL;
-  MenuIndex menu_index;
-
-  bool should_scroll = false;
-  if (browser != NULL && browser->menu_layer != NULL)
-  {
-    // menu is initialized so let's check if we should scroll
-    menu_layer = browser->menu_layer;
-    menu_index = menu_layer_get_selected_index(menu_layer);
-
-    should_scroll = s_horiz_scroll_scrolling_still_required && s_menu_selection_index == menu_index.row;
-  }
-
-  if (should_scroll)
-  {
-    // printf("scroll_callback ~> legit browser && menu layer!");
-
-    // TODO: check that the callback fired at the same index as current browser!
-
-    char *title = browser->menu_titles[menu_index.row];
-
-    printf("horiz scroll..: %s", title+s_horiz_scroll_offset);
-
-    // because in row==0, menu_selection_changed doesn't get called
-    // if (menu_index.row != 0)
-    // {
-    //   s_horiz_scroll_menu_reloading_to_scroll = true;
-    // }
-    s_horiz_scroll_scrolling_still_required = false;
-    // menu_layer_reload_data(menu_layer);
-    layer_mark_dirty(menu_layer_get_layer(menu_layer));
-    s_horiz_scroll_timer = app_timer_register(HORIZ_SCROLL_ITEM_TIME, horiz_scroll_callback, NULL); // FIXME
-    s_horiz_scroll_timer_active = true;
-  }
-  else
-  {
-    s_horiz_scroll_offset = 0;
-    // s_horiz_scroll_timer_active = false;
-    s_horiz_scroll_timer = app_timer_register(HORIZ_SCROLL_WAIT_TIME, selected_index_monitor, NULL); // FIXME
-  }
-}
-
-static void selected_index_monitor(void *data)
-{
-  // printf("monitor called"); // FIXME: useful for debugging, but need to take out at some point...
-
-  unsigned long timestamp = get_timestamp();
-  int difference = timestamp - s_horiz_scroll_timer_timestamp;
-  // printf("difference: %d, old: %lu, new: %lu", difference, s_horiz_scroll_timer_timestamp, timestamp);
-
-  MenuBrowser *browser = s_menu_browsers[s_browser_index];
-  MenuIndex menu_index;
-
-  bool should_scroll = false;
-  if (browser != NULL && browser->menu_layer != NULL)
-  {
-    // printf("legit browser & menu layer!");
-    menu_index = menu_layer_get_selected_index(browser->menu_layer);
-    s_menu_selection_index = menu_index.row;
-
-    if (s_horiz_scroll_timer_timestamp == 0)
-    {
-      s_horiz_scroll_timer_timestamp = timestamp;
-    }
-    else if (s_menu_selection_index != menu_index.row)
-    {
-      s_horiz_scroll_timer_timestamp = timestamp;
-    }
-
-    should_scroll = s_menu_selection_index == menu_index.row && s_horiz_scroll_menu_index != menu_index.row && difference >= HORIZ_SCROLL_WAIT_TIME;
-  }
-
-  if (should_scroll)
-  {
-    // start scrolling
-    s_horiz_scroll_scrolling_still_required = true;
-    s_horiz_scroll_timer_timestamp = timestamp;
-    s_horiz_scroll_offset = 0;
-    s_horiz_scroll_menu_index = menu_index.row;
-
-    horiz_scroll_callback(NULL);
-  }
-  else
-  {
-    // otherwise, keep checking the selected index
-    s_horiz_scroll_timer = app_timer_register(HORIZ_SCROLL_WAIT_TIME, selected_index_monitor, NULL); // FIXME
-  }
-}
-
-static void menu_selection_changed_callback(MenuLayer *menu_layer, MenuIndex new_index, MenuIndex old_index, void *data)
-{
-  s_horiz_scroll_offset = 0;
-  s_horiz_scroll_timer_timestamp = get_timestamp();
-  return;
-
-  // printf("selection changed %d -> %d (current = %d)", old_index.row, new_index.row, s_menu_selection_index);
-  // MenuBrowser *browser = s_menu_browsers[s_browser_index];
-
-  if (s_menu_selection_index == new_index.row)
-  {
-    // s_horiz_scroll_menu_reloading_to_scroll = true;
-    return;
-  }
-  s_menu_selection_index = new_index.row;
-  // s_horiz_scroll_offset = 0;
-
-  // printf("updated current = %d, reloading to scroll? %s, timer active? %s",
-  //   s_menu_selection_index,
-  //   "NA",
-  //   // s_horiz_scroll_menu_reloading_to_scroll ? "YES" : "NO",
-  //   s_horiz_scroll_timer_active ? "YES" : "NO");
-
-  // initiate_horiz_scroll_timer("menu_selection_changed_callback");
-
-  /*
-  if (s_horiz_scroll_menu_reloading_to_scroll)
-  {
-    s_horiz_scroll_menu_reloading_to_scroll = false;
-  }
-  else
-  {
-    initiate_horiz_scroll_timer("menu_selection_changed_callback");
-  }*/
-}
-
-static void menu_select_long_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data)
-{
-  printf("long click!");
-  // initiate_horiz_scroll_timer();
+  graphics_draw_text(ctx, subtitle,
+    fonts_get_system_font(FONT_KEY_GOTHIC_24), GRect(5, title_height + 1, cell_width, subtitle_height),
+    GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
 }
 
 static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data)
 {
-  printf("menu select");
-  printf("browser index: %d", s_browser_index);
   MenuBrowser *browser = s_menu_browsers[s_browser_index];
 
   if (browser->menu_num_entries == 0)
@@ -436,19 +238,8 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
     return;
   }
 
-  // if (s_horiz_scroll_timer_active)
-  // {
-  //   app_timer_cancel(s_horiz_scroll_timer);
-  //   s_horiz_scroll_timer_active = false;
-  // }
-  s_horiz_scroll_offset = 0;
-
-  s_horiz_scroll_menu_index = -1;
-  s_menu_selection_index = -1;
   menu_layer_reload_data(browser->menu_layer);
-  s_horiz_scroll_scrolling_still_required = false;
 
-  printf("inside if");
   char *msg = browser->msg;
   char *route = browser->route;
   char *direction = browser->direction;
@@ -459,7 +250,7 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
   char *selector = browser->menu_selectors[cell_index->row];
   char *new_msg = NULL;
   char **split = NULL; // in case we need to split things... need to free afterward
-  printf("starting strcmps w/ msg: %s", msg ? msg : "NULL");
+  // printf("starting strcmps w/ msg: %s", msg ? msg : "NULL");
 
   if (strcmp(msg, MSG_ROUTES) == 0)
   {
@@ -491,9 +282,7 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
   {
     new_msg = MSG_PREDICTIONS;
 
-    printf("selector: %s", selector ? selector : "NULL");
     split = str_split(selector, '_');
-    printf("done splitting!");
     if (split != NULL)
     {
       route = split[0];
@@ -515,8 +304,6 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
     }
   }
 
-  printf("lots'o stuff, new_msg: %s, route: %s", new_msg ? new_msg : "NULL", route ? route : "NULL");
-
   if (new_msg)
   {
     printf("msg: %s, route: %s, direction: %s, stopid: %s, stopname: %s: extra: %s",
@@ -527,9 +314,6 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
     stopname ? stopname : "NULL",
     extra ? extra : "NULL");
 
-    printf("browser index before: %d", s_browser_index);
-
-    printf("browser index after: %d", s_browser_index);
     push_menu(new_msg, route, direction, stopid, stopname, extra);
   }
   else if (split != NULL)
@@ -663,7 +447,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   {
     Window *window = browser->menu_window;
     Layer *window_layer = window_get_root_layer(window);
-    printf("num entries: %d", num_entries);
+    // printf("num entries: %d", num_entries);
     if (num_entries > 0)
     {
       browser->menu_num_entries = num_entries;
@@ -718,9 +502,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
           .get_cell_height = menu_get_cell_height_callback,
           .draw_header = menu_draw_header_callback,
           .draw_row = menu_draw_row_callback,
-          .select_click = menu_select_callback,
-          .select_long_click = menu_select_long_callback,
-          .selection_changed = menu_selection_changed_callback
+          .select_click = menu_select_callback
         });
 
         menu_layer_set_click_config_onto_window(menu_layer, window);
@@ -762,7 +544,6 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context)
 /*
  * WINDOW MANAGEMENT
  */
-
 
 static void initialize_browser(MenuBrowser *browser)
 {
@@ -845,7 +626,9 @@ static void window_load(Window *window)
 void reload_menu_browser_if_necessary()
 {
   // the browser that'll soon be on the top of the stack
+  // (s_browser_index should already be indexed to the backBrowser by now)
   MenuBrowser *backBrowser = s_menu_browsers[s_browser_index];
+
   if (backBrowser->loading_state != LOADING_DONE)
   {
     printf("back browser not finished loading!");
@@ -857,7 +640,6 @@ void reload_menu_browser_if_necessary()
       send_menu_app_message_helper,
       NULL);
   }
-  printf("msg: %s", backBrowser->msg ? backBrowser->msg : "NULL");
   if (strcmp(backBrowser->msg, MSG_FAVORITES) == 0 || strcmp(backBrowser->msg, MSG_COMMUTE) == 0)
   {
     printf("returning to favorites / commute menu!");
@@ -933,37 +715,12 @@ static void window_unload(Window *window)
 
   if (s_browser_index > 0)
   {
+    // index points to the browser that'll soon be on the top of the stack
     s_browser_index--;
     reload_menu_browser_if_necessary();
-
-    // // the browser that'll soon be on the top of the stack
-    // MenuBrowser *backBrowser = s_menu_browsers[s_browser_index];
-    // if (backBrowser->loading_state != LOADING_DONE)
-    // {
-    //   printf("back browser not finished loading!");
-    //   menu_layer_reload_data(backBrowser->menu_layer);
-    //   send_menu_app_message(s_menu_browsers[s_browser_index]);
-    // }
-    // printf("msg: %s", backBrowser->msg ? backBrowser->msg : "NULL");
-    // if (strcmp(backBrowser->msg, MSG_FAVORITES) == 0)
-    // {
-    //   printf("returning to favorites / commute menu!");
-    //   free_browser_lists(backBrowser);
-    //
-    //   layer_add_child(window_get_root_layer(backBrowser->menu_window),
-    //     text_layer_get_layer(get_text_layer_loading(NULL)));
-    //   send_menu_app_message(s_menu_browsers[s_browser_index]);
-    // }
   }
   else
   {
-    if (s_horiz_scroll_timer_active && s_horiz_scroll_timer != NULL)
-    {
-      app_timer_cancel(s_horiz_scroll_timer);
-      s_horiz_scroll_timer_active = false;
-    }
-    s_horiz_scroll_timer = NULL;
-
     free(s_menu_browsers);
     s_menu_browsers = NULL;
 
@@ -979,7 +736,6 @@ static void window_unload(Window *window)
 
 void menu_browser_register_app_message_callbacks()
 {
-  printf("~ registered menu browser app msg callbacks");
   app_message_register_inbox_received(inbox_received_callback);
   app_message_register_inbox_dropped(inbox_dropped_callback);
   app_message_register_outbox_failed(outbox_failed_callback);
@@ -991,7 +747,6 @@ void push_menu(char *msg, char *route, char *direction, char *stopid, char *stop
   printf("menu push");
   if (s_menu_browsers == NULL)
   {
-    printf("first push menu !");
     int num_browsers = 4;
     s_menu_browsers = calloc(num_browsers, sizeof(MenuBrowser *));
     s_browser_index = 0;
@@ -1003,21 +758,13 @@ void push_menu(char *msg, char *route, char *direction, char *stopid, char *stop
 
   menu_browser_register_app_message_callbacks();
 
-  MenuBrowser *browser = calloc(1, sizeof(MenuBrowser)); // s_menu_browsers[s_browser_index];
+  MenuBrowser *browser = calloc(1, sizeof(MenuBrowser));
   initialize_browser(browser);
   s_menu_browsers[s_browser_index] = browser;
-
-  // printf("created browser: %p", browser);
-  // for (int i=0; i<4; i++)
-  // {
-  //   printf("menu_browsers[%d] = %p", i, s_menu_browsers[i]);
-  // }
 
   // Create main Window element and assign to pointer
   Window *window = window_create();
   browser->menu_window = window;
-
-  // printf("created menu_window: %p", window);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(window, (WindowHandlers) {
@@ -1050,12 +797,8 @@ void push_menu(char *msg, char *route, char *direction, char *stopid, char *stop
   {
     browser->extra = strdup(extra);
   }
-  // send_menu_app_message(true);
 
-  printf("launching helper on timer");
-  printf("browser index... %d", s_browser_index);
-  // if (s_browser_index == 0)
-  // send_menu_app_message(true); // FIXME
+  printf("launching app msg on timer @ browser index: %d", s_browser_index);
   s_timer_browser_index = s_browser_index;
   s_timer = app_timer_register(
     250,
